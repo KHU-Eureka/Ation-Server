b9453e9bd16f007201b22229bba7cbbeaae7e162
seoh02h <seok02h@khu.ac.kr>
2022-03-02 23:26:08 +0900

    Fix whiteboard dto construct

--- src/main/java/com/eureka/ationserver/dto/whiteboard/WhiteboardRequest.java
+++ src/main/java/com/eureka/ationserver/dto/whiteboard/WhiteboardRequest.java
@@ -2,9 +2,11 @@ package com.eureka.ationserver.dto.whiteboard;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class WhiteboardRequest {
 
   private String whiteboard;
